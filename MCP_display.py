0af75c6995fb31a584fee81ded09e860851f515c
Charlie <charlie.leprince@institutoptique.fr>
2022-01-18 15:04:15 +0100

    plot mcp data

---
+++ MCP_display.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+
+
+import logging
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.optimize as opt
+from datetime import datetime
+from PyQt5.QtWidgets import QInputDialog
+from PyQt5.QtCore import Qt
+from PyQt5 import QtCore
+from HAL.gui.dataexplorer import getSelectionMetaDataFromCache
+
+logger = logging.getLogger(__name__)
+
+# /!\/!\/!\
+# in order to be imported as a user script, two "global" variables
+# have to be defined: NAME and CATEGORY
+NAME = "plot data"  # display name, used in menubar and command palette
+CATEGORY = "MCP"  # category (note that CATEGORY="" is a valid choice)
+
+
+def main(self):
+    """
+    the script also have to define a `main` function. When playing a script,
+    HAL runs `main` passes one (and only one) argument "self" that is the
+    HAL mainwindow object (granting access to all the gui attributes and methods)
+    """
+
+    # get metadata from current selection
+    metadata = getSelectionMetaDataFromCache(self, update_cache=True)
+
+    # -- get selected data
+    selection = self.runList.selectedItems()
+    if not selection:
+        return
+
+    # -- init object data
+    # get object data type
+    data_class = self.dataTypeComboBox.currentData()
+    data = data_class()
+    # get path
+    item = selection[0]
+    data.path = item.data(QtCore.Qt.UserRole)
+    X, Y, T = data.getrawdata()
+
+    T_min_ROI = 150
+    T_max_ROI = 165
+    T_ROI = T[(T > T_min_ROI) & (T < T_max_ROI)]
+    X_ROI = X[(T > T_min_ROI) & (T < T_max_ROI)]
+    Y_ROI = Y[(T > T_min_ROI) & (T < T_max_ROI)]
+
+    fig1 = plt.figure()
+    ax = plt.axes(projection="3d")
+    ax.scatter3D(X, Y, T, marker=".")
+    ax.scatter3D(X_ROI, Y_ROI, T_ROI, marker=".")
+    plt.xlabel("X")
+    plt.ylabel("Y")
+    fig1.show()
+
+    fig2 = plt.figure()
+    plt.hist(T, bins=np.linspace(0, 180, 300))
+    # plt.hist(T_ROI,bins=np.linspace(0,180,300))
+    plt.xlabel("time (ms)")
+    plt.ylabel("number of events")
+    plt.grid(True)
+    fig2.show()
+
+    fig3 = plt.figure()
+    plt.hist2d(X, Y, bins=np.linspace(-40, 40, 2 * 81), cmap=plt.cm.jet)
+    plt.xlabel("time (ms)")
+    plt.ylabel("number of events")
+    plt.colorbar()
+    fig3.show()
