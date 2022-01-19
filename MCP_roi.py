b1a57d4521aeb6d678042d34285b66490749461a
Charlie <charlie.leprince@institutoptique.fr>
2022-01-19 13:24:01 +0100

    wip

---
+++ MCP_roi.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+
+import logging
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.optimize as opt
+from datetime import datetime
+from PyQt5.QtWidgets import QInputDialog
+from PyQt5.QtCore import Qt
+from PyQt5 import QtCore
+from HAL.gui.dataexplorer import getSelectionMetaDataFromCache
+
+logger = logging.getLogger(__name__)
+
+# /!\/!\/!\
+# in order to be imported as a user script, two "global" variables
+# have to be defined: NAME and CATEGORY
+NAME = "ROI"  # display name, used in menubar and command palette
+CATEGORY = "MCP"  # category (note that CATEGORY="" is a valid choice)
+
+
+def main(self):
+    """
+    the script also have to define a `main` function. When playing a script,
+    HAL runs `main` passes one (and only one) argument "self" that is the
+    HAL mainwindow object (granting access to all the gui attributes and methods)
+    """
+
+    # get metadata from current selection
+    metadata = getSelectionMetaDataFromCache(self, update_cache=True)
+
+    # -- get selected data
+    selection = self.runList.selectedItems()
+    if not selection:
+        return
+
+    # -- init object data
+    # get object data type
+    data_class = self.dataTypeComboBox.currentData()
+    data = data_class()
+    # get path
+    print(len(selection))
+    item = selection[0]
+    data.path = item.data(QtCore.Qt.UserRole)
+    print(data.path)
+    X, Y, T = data.getrawdata()
+
+    user_name, ok = QInputDialog.getText(
+        self, "Hello there", "what's your name ?", text="Dave"
+    )
