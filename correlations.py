7c91298b01670656b2d80e898e55a955904ee3fd
victor <victor.gondret@institutoptique.fr>
2022-05-13 14:26:46 +0200

    Faster computation : wip

--- correlations.py
+++ correlations.py
@@ -14,9 +14,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from alive_progress import alive_bar
+from tqdm import tqdm, trange
 
 
-class Correlation:
+class CorrelationNotebook:
     """
     Classe corrélation. Prend en entrée un dataframe avec X, Y et T et calcule de corrélations etc...
 
@@ -80,6 +81,7 @@ class Correlation:
         self.gravity = 9.81
         self.var1 = None
         self.var2 = None
+        self.remove_shot_noise = True
         self.ROI = {}  # Region Of Interest
         self.ROD = {}  # Region Of Desinterest.
         self.boxes = {
@@ -146,7 +148,7 @@ class Correlation:
     def apply_ROI(self):
         """
         Modifie le dataframe "atoms" en appliquant la ROI. Cela permet d'alléger les données à traiter.
-        Si la ROI est vide, la méthode ne fait rien. Le format de la ROI doit être
+        Si la ROI est vide, la méthode ne fait rien. Le format de la ROI doit être {"Vx": {"max":120, "min":-120}}
         """
         if self.ROI:
             for key, entry in self.ROI.items():
@@ -170,7 +172,18 @@ class Correlation:
                     )
                 ]
 
-    def obtain_number_of_atoms_per_cycle_in_box(self, df, box, column_name="N1"):
+    def merge_dataframe_on_cycles(self, df1, df2):
+        """
+        Merge 2 dataframe sur l'entête "Cycle". Met 0 si le df2 n'a pas de valeur à ce cycle.
+        """
+        df_merged = df1.merge(
+            df2, how="outer", on="Cycle"
+        )  # l'option "outer" permet de conserver les cycles où
+        # il n'y a pas d'atomes. Pandas ajoute donc un NaN à la place.
+        df_merged = df_merged.fillna(0)
+        return df_merged
+
+    def obtain_number_of_atoms_per_cycle_in_box(self, df, box, column_name="N_1"):
         """
         Retourne un dataframe de deux colonnes : une colonne avec le numéro du cycle (nom de colonne "Cycle") et une colonne avec nombre d'atomes compté dans la boite au cycle correspondant (nom de colonne column_name)
 
@@ -191,9 +204,16 @@ class Correlation:
         # "value_counts". This method returns a serie and not a dataframe and I prefer retranform it
         # into a dataframe.
         my_serie = df.value_counts(subset="Cycle")
+
         atoms_in_box = my_serie.to_frame()
         atoms_in_box = atoms_in_box.rename(columns={0: column_name})
         atoms_in_box.reset_index(inplace=True)
+        # atoms_in_box is now a dataframe with two columns "Cycle" and "N_1" (or column_name). However, if there were no atom at cycle 34 in the box, this cycle does not appear inside atoms_in_box. In order to have the number of atoms in the box at each cycle, we must add 0 to those cycles which does not appear.
+        # cycle_dataframe is just a dataframe with n_cycles : we use it to merge and add zeros to atoms_in_box
+        cycle_dataframe = pd.DataFrame(
+            np.arange(1, self.n_cycles + 1, 1), columns=["Cycle"]
+        )
+        atoms_in_box = self.merge_dataframe_on_cycles(cycle_dataframe, atoms_in_box)
         return atoms_in_box
 
     def counts_atoms_in_boxes_one_variable(self, df, var, box, column_name="N_1"):
@@ -215,95 +235,23 @@ class Correlation:
                 column_name (ex : "N1") : avec le nombre d'atome dans la boîte
                 var.name (ex : "ΔVx"): avec la valeur de la position/taille de la boîte.
         """
-        # Petite bar pour faire patienter
-        with alive_bar(var.n_step, title="Gathering {}".format(var.name)) as bar:
-            # On parcourt les différentes valeurs de la Variable var
-            for i in range(var.n_step):
-                # On change la boîte selon la i-ème valeur de var
-                box[var.axe][var.type] = var.get_value_i(i)
-                # On récupère le dataframe avec le nombre d'atome dans la boîte à chaque cycle
-                dataframe = self.obtain_number_of_atoms_per_cycle_in_box(
-                    df, box, column_name=column_name
-                )
-                dataframe[var.name] = var.get_value_i(i)
-                # On ajoute la colonne avec la valeur de variable
-                if i == 0:
-                    result = dataframe
-                else:
-                    result = pd.concat([result, dataframe])
-                bar()
+        # On parcourt les différentes valeurs de la Variable var (tqdm --> waiting bar)
+        for i in tqdm(range(var.n_step), desc="Gathering {}".format(var.name)):
+            # On change la boîte selon la i-ème valeur de var
+            box[var.axe][var.type] = var.get_value_i(i)
+            # On récupère le dataframe avec le nombre d'atome dans la boîte à chaque cycle
+            dataframe = self.obtain_number_of_atoms_per_cycle_in_box(
+                df, box, column_name=column_name
+            )
+            dataframe[var.name] = var.get_value_i(i)
+            # On ajoute la colonne avec la valeur de variable
+            if i == 0:
+                result = dataframe
+            else:
+                result = pd.concat([result, dataframe])
         # les index se répètent : c'est bof : je les réinitialise.
         result.reset_index(drop=True)
         return result
-
-    def quantity_of_interest(self, dataframe=""):
-        """
-        Prend en argument un dataframe dont les colonnes sont le nombre d'atomes dans les boîtes 1 et 2 à chaque cycle1 Retourne une liste de flottants avec les différentes valeurs d'intérêt. Si dataframe = "", renvoie une liste de strings avec le nom de chaque quantité calculée.
-        Pour rajouter des valeurs d'intérêt à calculer, il suffit de rajouter dans la liste column_names le nom de la variable et de l'ajouter dans result.
-
-        Parameters
-        ----------
-        dataframe : pandas dataframe avec (au moins) 3 colonnes
-                "Cycle" le numéro de cycle
-                "N_1", avec le nombre d'atomes dans la boîte 1
-                "N_2", avec le nombre d'atomes dans la boîte 2
-
-        Returns
-        ----------
-        column_names (si aucun dataframe n'est donné) : liste de string avec le nom des valeurs calculées
-        computed_quantities (si un dataframe est donné) : liste de flottants avec la quantité d'intérêt calculée.
-        """
-        column_names = [
-            "N_1",
-            "N_2",
-            "N_1+N_2",
-            "variance",
-            "normalized variance",
-            "g^2",
-        ]
-
-        if type(dataframe) == str:
-            return column_names
-
-        N1 = np.sum(dataframe["N_1"]) / self.n_cycles
-        N2 = np.sum(dataframe["N_2"]) / self.n_cycles
-
-        variance = (
-            np.sum((dataframe["N_1"] - dataframe["N_2"]) ** 2) / self.n_cycles
-            - (np.sum(dataframe["N_1"] - dataframe["N_2"]) / self.n_cycles) ** 2
-        )
-        normalized_variance = variance / (N1 + N2)
-
-        # Calcul de g2
-        numerator = np.sum(dataframe["N_1"] * dataframe["N_2"]) / self.n_cycles
-        denominator = (
-            np.sum(dataframe["N_1"]) * np.sum(dataframe["N_2"]) / (self.n_cycles ** 2)
-        )
-        if denominator > 0:
-            g2 = numerator / denominator
-        else:
-            g2 = 1
-
-        computed_quantities = [
-            N1,
-            N2,
-            N1 + N2,
-            variance,
-            normalized_variance,
-            g2,
-        ]
-        return computed_quantities
-
-    def merge_dataframe_on_cycles(self, df1, df2):
-        """
-        Merge 2 dataframe sur les cycles. Met 0 si le df2 n'a pas de valeur à ce cycle.
-        """
-        df_merged = df1.merge(
-            df2, how="outer", on="Cycle"
-        )  # l'option "outer" permet de conserver les cycles où
-        # il n'y a pas d'atomes. Pandas ajoute donc un NaN à la place.
-        df_merged = df_merged.fillna(0)
-        return df_merged
 
     def compute_correlations_different_box_scanned(self):
         """
@@ -312,7 +260,7 @@ class Correlation:
         #### STEP 1 : on récupère le nombre d'atome dans les boites associées à var1 et var2. Disons que intuivement, var1 corresponde à la boîte 1 et var2 à la boîte 2 mais ce n'est pas nécessaire dans le code.
         # --> Start with var1
         # On ne récupère que les atomes présents dans la boîte selon les deux axes qui ne varient pas. Par exemple, si on est en train de faire varier la position de la boite selon Vx, on récupère les atomes qui vérifient déjà les bonnes conditions selon Vy et Vz pour alléger les calculs.
-        box = self.boxes[self.var1.box]
+        box = self.boxes[self.var1.box].copy()
         # On enlève l'axe concerné par le scan
         posi_and_size = box.pop(self.var1.axe)
         # l'axe concerné par le scan est donc
@@ -323,7 +271,7 @@ class Correlation:
             df_atoms_var1, self.var1, scanned_box, column_name="N_" + self.var1.box
         )
         # --> Do the same with var2
-        box = self.boxes[self.var2.box]
+        box = self.boxes[self.var2.box].copy()
         posi_and_size = box.pop(self.var2.axe)
         scanned_box = {self.var2.axe: posi_and_size}
         df_atoms_var2 = self.get_atoms_in_box(self.atoms, box)
@@ -367,7 +315,7 @@ class Correlation:
         Calcule des corrélations self.result lorsqu'une seule boîte est scannée. La démarche est la même que celle de compute_correlations_different_box_scanned
         """
         # On ne récupère que les atomes présents dans la boîte selon les deux axes qui ne varient pas. Par exemple, si on est en train de faire varier la position de la boite selon Vx, on récupère les atomes qui vérifient déjà les bonnes conditions selon Vy et Vz pour alléger les calculs.
-        box = self.boxes[var.box]
+        box = self.boxes[var.box].copy()
         # On enlève l'axe concerné par le scan
         posi_and_size = box.pop(var.axe)
         # l'axe concerné par le scan est donc
@@ -585,6 +533,152 @@ class Correlation:
         ax.invert_yaxis()
         plt.title(z)
         plt.show()
+
+    def quantity_of_interest(self, dataframe=""):
+        """
+        Prend en argument un dataframe dont les colonnes sont le nombre d'atomes dans les boîtes 1 et 2 à chaque cycle1 Retourne une liste de flottants avec les différentes valeurs d'intérêt. Si dataframe = "", renvoie une liste de strings avec le nom de chaque quantité calculée.
+        Pour rajouter des valeurs d'intérêt à calculer, il suffit de rajouter dans la liste column_names le nom de la variable et de l'ajouter dans result.
+
+        Parameters
+        ----------
+        dataframe : pandas dataframe avec (au moins) 3 colonnes
+                "Cycle" le numéro de cycle
+                "N_1", avec le nombre d'atomes dans la boîte 1
+                "N_2", avec le nombre d'atomes dans la boîte 2
+
+        Returns
+        ----------
+        column_names (si aucun dataframe n'est donné) : liste de string avec le nom des valeurs calculées
+        computed_quantities (si un dataframe est donné) : liste de flottants avec la quantité d'intérêt calculée.
+        """
+        column_names = [
+            "N_1",
+            "N_2",
+            "N_1+N_2",
+            "variance",
+            "normalized variance",
+            "g^2",
+        ]
+
+        if type(dataframe) == str:
+            return column_names
+
+        N1 = np.sum(dataframe["N_1"]) / self.n_cycles
+        N2 = np.sum(dataframe["N_2"]) / self.n_cycles
+
+        variance = (
+            np.sum((dataframe["N_1"] - dataframe["N_2"]) ** 2) / self.n_cycles
+            - (np.sum(dataframe["N_1"] - dataframe["N_2"]) / self.n_cycles) ** 2
+        )
+        normalized_variance = variance / (N1 + N2)
+
+        # Calcul de g2
+        numerator = np.sum(dataframe["N_1"] * dataframe["N_2"]) / self.n_cycles
+        denominator = (
+            np.sum(dataframe["N_1"]) * np.sum(dataframe["N_2"]) / (self.n_cycles**2)
+        )
+        if denominator > 0:
+            g2 = numerator / denominator
+        else:
+            g2 = 1
+
+        computed_quantities = [
+            N1,
+            N2,
+            N1 + N2,
+            variance,
+            normalized_variance,
+            g2,
+        ]
+        return computed_quantities
+
+    def compute_result(self, total):
+        """
+        Calcul le dataframe de corrélation result utilisant total. Total est un dataframe avec pour chaque cycle et chaque position/taille de boites le nombre d'atomes dans la boite 1 et dans la boite 2.
+
+        /!\ /!\ Ne fonctionne que si on a deux variables --> à faire pour un scan 1D ? ou bien on garde la façon naïve car ce n'est pas long.
+
+        Parameters
+        ----------
+        total : pandas dataframe de 5 colonnes : 'Cycle' le cycle, 'N_1' et 'N_2' nombre d'atomes dans la boîte 1 et 2, self.var1 et self.var2 la position/taille des boîtes lors du scan. Le nombre de lignes de total est dont Nombre_de_cycles x Nombre_de_différentes_var1 x Nombre_de_différentes_var2.
+
+
+        Built
+        ----------
+        self.result : pandas dataframe de Nombre_de_différentes_var1 x Nombre_de_différentes_var2 lignes. Ses différentes colonnes sont
+            var1.name et var2.name : le nom des variables scannées
+            "N_1", "N_2" :  moyenne sur les cycles du nombre d'atomes dans la boîte 1, 2
+            "N_1+N_2" : somme des populations moyenne
+            "N_1-N_2" : différence des populations moyenne
+            "(N_1-N_2)^2" : moyenne (sur les cycles) des écart quadratiques
+            "N_1*N_2" : moyenne du produit du nombre d'atomes
+            "variance" : variance, il s'agit de la quantité <(N1 - N2)^2> / <N1-N2>^2 où la moyenne est prise sur les cycles.
+            "normalized variance" : variance normalisée, variance divisée par la somme des populations  : variance / <N1 + N2>
+        """
+        total["N_1*N_2"] = total["N_1"] * total["N_2"]
+        total["N_1-N_2"] = total["N_1"] - total["N_2"]
+        total["(N_1-N_2)^2"] = (total["N_1"] - total["N_2"]) ** 2
+        # on somme les données sur les cycles (on les groupe donc par différentes valeurs de var1 et var2)
+        self.result = total.groupby(
+            [self.var1.name, self.var2.name], as_index=False
+        ).sum()
+        self.result["variance"] = (
+            self.result["(N_1-N_2)^2"] / self.n_cycles
+            - (self.result["N_1-N_2"] / self.n_cycles) ** 2
+        )
+
+        self.result["g^2"] = (
+            self.result["N_1*N_2"]
+            / (self.result["N_1"] * self.result["N_2"])
+            * self.n_cycles
+        )
+        # Pour l'instant, N_1, N_2 et N_1*N_2 sont des sommes et non des moyennes : on les moyenne donc.
+        self.result["N_1"] /= self.n_cycles
+        self.result["N_2"] /= self.n_cycles
+        self.result["N_1*N_2"] /= self.n_cycles
+        self.result["N_1-N_2"] /= self.n_cycles
+        self.result["(N_1-N_2)^2"] /= self.n_cycles
+        self.result["N_1+N_2"] = self.result["N_1"] + self.result["N_2"]
+
+        self.result["normalized variance"] = (
+            self.result["variance"] / self.result["N_1+N_2"]
+        )
+        # on enlève le shot noise si cela est demandé par l'utilisateur.
+        if self.remove_shot_noise:
+            print("I am taking off shot noise")
+            local_condition = self.result[self.var1.name] == self.result[self.var1.name]
+
+    def compute_correlations_different_box_scanned_fast(self):
+        """
+        Méthode pour calcul des corrélations lorsque var1 et var2 (les paramètres scannés) correspondent à deux boites différentes.
+        """
+        #### STEP 1 : on récupère le nombre d'atome dans les boites associées à var1 et var2. Disons que intuivement, var1 corresponde à la boîte 1 et var2 à la boîte 2 mais ce n'est pas nécessaire dans le code.
+        # --> Start with var1
+        # On ne récupère que les atomes présents dans la boîte selon les deux axes qui ne varient pas. Par exemple, si on est en train de faire varier la position de la boite selon Vx, on récupère les atomes qui vérifient déjà les bonnes conditions selon Vy et Vz pour alléger les calculs.
+        box = self.boxes[self.var1.box].copy()
+        # On enlève l'axe concerné par le scan
+        posi_and_size = box.pop(self.var1.axe)
+        # l'axe concerné par le scan est donc
+        scanned_box = {self.var1.axe: posi_and_size}
+        # On a donc deux boîtes maintenant : une avec deux axes (box) qui ne sont pas modifié à chaque position/taille de boîte et une autre (scanned_box) avec un seul axe qui correspond à l'axe scanné var1.axe
+        df_atoms_var1 = self.get_atoms_in_box(self.atoms, box)
+        # Result var1 est un dataframe avec le nombre d'atomes dans la boîte à chaque cycle pour les différentes positions de la boîte. Il a donc 3 colonnes dont les entêtes sont "Cycles", "N_i" avec i = 1 ou 2 selon la boîte et le nom de la variables scannée par exemple "ΔVx". Voir la documentation de counts_atoms_in_boxes_one_variable pour plus de détails.
+        result_var1 = self.counts_atoms_in_boxes_one_variable(
+            df_atoms_var1, self.var1, scanned_box, column_name="N_" + self.var1.box
+        )
+        # --> Do the same with var2
+        box = self.boxes[self.var2.box].copy()
+        posi_and_size = box.pop(self.var2.axe)
+        scanned_box = {self.var2.axe: posi_and_size}
+        df_atoms_var2 = self.get_atoms_in_box(self.atoms, box)
+        result_var2 = self.counts_atoms_in_boxes_one_variable(
+            df_atoms_var2, self.var2, scanned_box, column_name="N_" + self.var2.box
+        )
+        #### STEP2
+        # On construit le dataframe total, qui initialement contient 5 colonnes : Cycle le cycle, N_1 et N_2 nombre d'atomes dans la boîte 1 et 2, self.var1 et self.var2 la position/taille des boîtes lors du scan. Le nombre de lignes de total est dont Nombre_de_cycles x Nombre_de_différentes_var1 x Nombre_de_différentes_var2.
+        total = pd.merge(result_var1, result_var2, on="Cycle")
+
+        #### STEP3 : computes quantites of interest
 
 
 class Variable:
